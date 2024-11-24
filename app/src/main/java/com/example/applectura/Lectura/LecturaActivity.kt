e801bf08ec2509ff66ee40ae990b753c34cfd5a0
angel5000 <delllancer@gmail.com>
2024-11-23 23:18:33 -0500

    seccion lectura info agregada

--- app/src/main/java/com/example/applectura/Lectura/LecturaActivity.kt
+++ app/src/main/java/com/example/applectura/Lectura/LecturaActivity.kt
@@ -1,12 +1,28 @@
 package com.example.applectura.Lectura
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
 import com.example.applectura.R
 
 class LecturaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lectura)
+        setContentView(R.layout.activity_lectura2)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        // Habilitar el botón de retroceso si es necesario
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
     }
+    override fun onSupportNavigateUp(): Boolean {
+        val intent = Intent(this, PrincipalActivity::class.java)
+        startActivity(intent)
+        finish() // Finaliza la actividad actual para que no quede en la pila
+        return true
+    }
+
 }
